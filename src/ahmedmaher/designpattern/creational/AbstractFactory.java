70f9fe8a54c3ea6e1104dc5bd4df102f67405200
Ahmed Maher <ahmededu015@gmail.com>
2023-12-31 20:51:06 +0200

    Change the example of Abstract Factory DP.

--- src/ahmedmaher/designpattern/creational/AbstractFactory.java
+++ src/ahmedmaher/designpattern/creational/AbstractFactory.java
@@ -2,65 +2,105 @@ package ahmedmaher.designpattern.creational;
 
 public class AbstractFactory {
     public static void main(String[] args) {
-        ModernFurnitureFactory modernFurnitureFactory = new ModernFurnitureFactory();
-        VictorianFurnitureFactory victorianFurnitureFactory = new VictorianFurnitureFactory();
+        GUIClient guiClient1 = new GUIClient(new WindowsFactory());
+        GUIClient guiClient2 = new GUIClient(new MacOSFactory());
 
-        Chair modernChair =  modernFurnitureFactory.createChair();
-        Chair victorianChair =  victorianFurnitureFactory.createChair();
-
-        modernChair.sitOn();
-        victorianChair.sitOn();
-
-
-
+        guiClient1.paint();
+        guiClient2.paint();
     }
 }
 
-
-interface FurnitureFactory {
-    Chair createChair();
+// Abstract Product
+interface IButton {
+    void click();
 }
 
-class ModernFurnitureFactory implements FurnitureFactory {
+interface ICheckBox {
+    void check();
+}
+
+// Concrete Abstract Products
+class WindowsButton implements IButton {
+
     @Override
-    public Chair createChair() {
-        return new ModernChair();
+    public void click() {
+        System.out.println("Windows Button clicked.");
     }
 }
 
-class VictorianFurnitureFactory implements FurnitureFactory {
+class WindowsCheckBox implements ICheckBox {
+
     @Override
-    public Chair createChair() {
-        return new VictorianChair();
+    public void check() {
+        System.out.println("Windows Checkbox checked.");
     }
 }
 
-interface Chair {
-    boolean hasLegs();
-    void sitOn();
-}
-
-class ModernChair implements Chair {
-    @Override
-    public boolean hasLegs() {
-        return true;
-    }
+class MacOSButton implements IButton {
 
     @Override
-    public void sitOn() {
-        System.out.println("You can sit on this modern chair.");
+    public void click() {
+        System.out.println("MacOS Button clicked.");
     }
 }
 
-class VictorianChair implements Chair {
+class MacOSCheckBox implements ICheckBox {
+
     @Override
-    public boolean hasLegs() {
-        return true;
+    public void check() {
+        System.out.println("MacOS Checkbox checked.");
+    }
+}
+
+// Abstract Factory
+
+interface IAbstractFactory {
+    IButton createButton();
+    ICheckBox createCheckBox();
+}
+
+// Concrete Abstract Factory
+class WindowsFactory implements IAbstractFactory {
+
+    @Override
+    public IButton createButton() {
+        return new WindowsButton();
     }
 
     @Override
-    public void sitOn() {
-        System.out.println("You can sit on this victorian chair.");
+    public ICheckBox createCheckBox() {
+        return new WindowsCheckBox();
+    }
+}
+
+class MacOSFactory implements IAbstractFactory {
+
+    @Override
+    public IButton createButton() {
+        return new MacOSButton();
+    }
+
+    @Override
+    public ICheckBox createCheckBox() {
+        return new MacOSCheckBox();
+    }
+}
+
+// Client
+class GUIClient {
+    private IAbstractFactory factory;
+
+    public GUIClient(IAbstractFactory factory) {
+        this.factory = factory;
+    }
+
+    void paint() {
+        IButton button = factory.createButton();
+        ICheckBox checkBox = factory.createCheckBox();
+
+        button.click();
+        checkBox.check();
+
     }
 }
 
